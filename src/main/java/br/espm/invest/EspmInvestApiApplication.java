08bc03bdea238989665f7513990df80ffb429f64
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2021-05-27 11:28:57 -0300

    spring

---
+++ src/main/java/br/espm/invest/EspmInvestApiApplication.java
@@ -0,0 +1,13 @@
+package br.espm.invest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspmInvestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspmInvestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/espm/invest/EspmInvestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.espm.invest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspmInvestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
